26813d0b20a9d820e46a2334fd4a213e6bfdbb87
SebastianForslund <sebastian.david.forslund@gmail.com>
2021-02-24 16:16:29 +0100

    Edited MailHandler.java so that the code follows the java conventions. Added a comment. Changed return type to void accoring to the STLDD

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/MailHandler.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/MailHandler.java
@@ -1,47 +1,41 @@
-
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
-public class MailHandler {
-	public final String senderEmail = "testingemailsender44@gmail.com";
-    public final String password = "dogsandcats123";
-    private Properties prop;
-	
+public class MailHandler 
+{
+	/*
+	IN ORDER FOR THIS CLASS TO FUNCTION, TWO EXTERNAL JARS ARE REQUIRED.
+	ADD ALL JARS IN THE "resources" FOLDER TO CLASSPATH (SIMILARLY TO HOW servlet-api.jar WAS ADDED).
+	*/
+	private final String senderEmail = "testingemailsender44@gmail.com";
+    private final String senderPassword = "dogsandcats123";
+    private Properties prop_;
 	public MailHandler() {
-		this.prop = new Properties();
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "587");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true"); //TLS
-        
+		this.prop_ = new Properties();
+        prop_.put("mail.smtp.host", "smtp.gmail.com");
+        prop_.put("mail.smtp.port", "587");
+        prop_.put("mail.smtp.auth", "true");
+        prop_.put("mail.smtp.starttls.enable", "true"); //TLS
 	}
-	
-	public boolean sendPasswordMail(String recipient, String username, String password) {
-		Session session = Session.getInstance(prop,
+	public void sendPasswordMail(String recipient, String username, String password) {
+		Session session = Session.getInstance(prop_,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("testingemailsender44@gmail.com", "dogsandcats123");
+                        return new PasswordAuthentication("senderEmail", "senderPassword");
                     }
                 });
-
         try {
-
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("from@gmail.com"));
             message.setRecipients(
                     Message.RecipientType.TO,
-                    InternetAddress.parse("TestingEmailSender44@gmail.com, sebastian.david.forslund@gmail.com")
+                    InternetAddress.parse("senderEmail, recipient")
             );
             message.setSubject("Your TimeMate Login Credentials");
             message.setText("Your login credentials are:" + "\n" + "Username: " + username + "\n" + "Password: " + password);
-
             Transport.send(message);
-
-            return true;
-
         } catch (MessagingException e) {
         	System.out.println(e.toString());
-        	return false;
         }
 	}
 }
