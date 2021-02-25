18f5da5e8af31bd7da6f22e7bad8043dbb4d206d
Anna Bergvall <an7151be-s@student.lu.se>
2021-02-25 09:10:24 +0100

    Added javadocs comments and method stems
    addUser, constructor, removeUser, setProjectLeader, connect, disconnect

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/DataBase.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/DataBase.java
@@ -1,4 +1,48 @@
+import java.sql.DriverManager;
 
 public class DataBase {
-
+	private DriverManager connection;
+	
+	public DataBase() {
+		
+	}
+	
+	public boolean connect() {
+		return false;
+	}
+	
+	public boolean disconnect() {
+		return false;
+	}
+	
+	// Methods only the admin has access to.
+	
+	/**
+	 * Only the admin can perform this action.
+	 * @return true if the user was successfully added to the database.
+	 */
+	public boolean addUser() {
+		return false;
+	}
+	
+	/**
+	 * Only the admin can perform this action.
+	 * @return true if the user was successfully removed from the database.
+	 */
+	public boolean removeUser() {
+		return false;
+	}
+	
+	/**
+	 * Associates the role of "project leader" to one user. Only the admin has
+	 * authority to perform this action.
+	 * @return true if a project leader was successfully assigned.
+	 */
+	public boolean setProjectLeader() {
+		return false;
+	}
+	
+	// Methods only the project leader has access to
+	
+	// Methods every user has access to
 }
