f7b0f9f1a7046995965298fc9081875ef6e0c799
SebastianForslund <sebastian.david.forslund@gmail.com>
2021-02-25 15:42:26 +0100

    First draft of PasswordChangerServlet.java

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/PasswordChangerServlet.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/PasswordChangerServlet.java
@@ -6,28 +6,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class PasswordChangerServlet
  */
 @WebServlet("/PasswordChangerServlet")
-public class PasswordChangerServlet extends HttpServlet {
+public class PasswordChangerServlet extends servletBase 
+{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PasswordChangerServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		DataBase db = new DataBase();
+		
+		PasswordHandler ph = new PasswordHandler();
+		if (db.setPassword(ph.hashPassword(session.getAttribute("password")))) {//Gets the password attribute, hashes the password, sets the password.
+			//change successful
+			request.setAttribute("errorMessage", true);
+		} else {
+			//change not successful
+			request.setAttribute("errorMessage", false);
+		}
 	}
 
 	/**
