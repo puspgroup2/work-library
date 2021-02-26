094233f1aa850998d45d9dda4fd7da665b0f3f96
Annelie <75272048+Annelizee@users.noreply.github.com>
2021-02-26 14:47:12 +0100

    Updated create_tables.sql

--- P3-Baseline-Informal/PUSS214205_SDDD/create_tables.sql
+++ P3-Baseline-Informal/PUSS214205_SDDD/create_tables.sql
@@ -1,3 +1,11 @@
+CREATE TABLE Users (
+userName varChar(20) NOT NULL,
+password varChar(50) NOT NULL,
+email varChar(100),
+role varChar(30),
+PRIMARY KEY(userName)
+);
+
 CREATE TABLE TimeReports (
 reportID Integer NOT NULL,
 userName varChar(20) NOT NULL,
@@ -9,4 +17,89 @@ FOREIGN KEY(userName) REFERENCES Users(userName)
 ON UPDATE CASCADE ON DELETE CASCADE,
 FOREIGN KEY(signature) REFERENCES Users(userName)
 ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+CREATE TABLE ActivityReports (
+reportID Integer NOT NULL,
+totalMinutes Integer,
+functionalTest Integer,
+systemTest Integer,
+regressionTest Integer,
+meeting Integer,
+lecture Integer,
+exercise Integer,
+computerExercise Integer,
+homeReading Integer,
+other Integer,
+PRIMARY KEY(reportID),
+FOREIGN KEY(reportID) REFERENCES TimeReports(reportID)
+ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE DocumentTimeD (
+reportID Integer NOT NULL,
+totalMinutes Integer,
+SDP Integer,
+SRS Integer,
+SVVS Integer,
+STLDD Integer,
+SVVI Integer,
+SDDD Integer,
+SVVR Integer,
+SSD Integer,
+finalReport Integer,
+PRIMARY KEY(reportID),
+FOREIGN KEY(reportID) REFERENCES TimeReports(reportID)
+ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE DocumentTimeI (
+reportID Integer NOT NULL,
+totalMinutes Integer,
+SDP Integer,
+SRS Integer,
+SVVS Integer,
+STLDD Integer,
+SVVI Integer,
+SDDD Integer,
+SVVR Integer,
+SSD Integer,
+finalReport Integer,
+PRIMARY KEY(reportID),
+FOREIGN KEY(reportID) REFERENCES TimeReports(reportID)
+ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE DocumentTimeR (
+reportID Integer NOT NULL,
+totalMinutes Integer,
+SDP Integer,
+SRS Integer,
+SVVS Integer,
+STLDD Integer,
+SVVI Integer,
+SDDD Integer,
+SVVR Integer,
+SSD Integer,
+finalReport Integer,
+PRIMARY KEY(reportID),
+FOREIGN KEY(reportID) REFERENCES TimeReports(reportID)
+ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE DocumentTimeF (
+reportID Integer NOT NULL,
+totalMinutes Integer,
+SDP Integer,
+SRS Integer,
+SVVS Integer,
+STLDD Integer,
+SVVI Integer,
+SDDD Integer,
+SVVR Integer,
+SSD Integer,
+finalReport Integer,
+PRIMARY KEY(reportID),
+FOREIGN KEY(reportID) REFERENCES TimeReports(reportID)
+ON UPDATE CASCADE ON DELETE CASCADE
 );
