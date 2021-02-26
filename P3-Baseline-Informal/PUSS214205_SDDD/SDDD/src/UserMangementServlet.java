58ab7bb39736d128cf3bb52fbfb5b0cb80a71205
HjalleNalle <78014028+HjalleNalle@users.noreply.github.com>
2021-02-26 13:34:42 +0100

    Started making the UserManagementServlet

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/UserMangementServlet.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/UserMangementServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class UserMangementServlet
@@ -13,21 +14,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UserMangementServlet")
 public class UserMangementServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserMangementServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		DataBase db = new DataBase();
+		HttpSession session = request.getSession();
+		UserManagementBean umb = new UserManagementBean();
+		db.addUser(request.getParameter("userName"), request.getParameter("password"), request.getParameter("email"));	
+		umb.populateBean(request, response);
+		response.sendRedirect("usermanagement.jsp");
+		
 	}
 
 	/**
