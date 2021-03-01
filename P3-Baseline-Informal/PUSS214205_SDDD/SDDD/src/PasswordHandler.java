333091bef8158c7fe5e784d8f41af66e7c92b7d2
UG_Oscar Johansson <os3111jo-s@student.lu.se>
2021-03-01 14:30:35 +0100

    Added hashPassword() method

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/PasswordHandler.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/PasswordHandler.java
@@ -1,8 +1,24 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class PasswordHandler {
 	
 	public String hashPassword(String password) {
-		
+		String hashedPassword = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+			StringBuilder sb = new StringBuilder();
+		    for(int i=0; i < bytes.length; i++){
+		    	sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+		    }
+		    hashedPassword = sb.toString();
+		}
+		catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return hashedPassword;
 	}
 	
 	public String generatePassword() {
