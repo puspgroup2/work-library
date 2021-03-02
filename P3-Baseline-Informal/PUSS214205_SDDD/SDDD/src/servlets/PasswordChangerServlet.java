25ac3d8c573311c723cb8e30741e1394c9d5e648
SebastianForslund <sebastian.david.forslund@gmail.com>
2021-03-02 10:18:16 +0100

    Added funcitonality to PasswordChangerServlet.java

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/PasswordChangerServlet.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/PasswordChangerServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import beans.UserBean;
 import database.DataBase;
 import handlers.PasswordHandler;
 
@@ -19,31 +20,45 @@ import handlers.PasswordHandler;
 public class PasswordChangerServlet extends ServletBase 
 {
 	private static final long serialVersionUID = 1L;
+	private final int PW_CHANGE_SUCCESSFUL_ = 2;
+	private final int PW_CHANGE_FAILED_NETWORK_ERROR_ = 1;
+	private final int PW_CHANGE_FAILED_IDENTICAL_PASSWORDS_ = 0;
+	private final int PW_CHANGE_FAILED_FALSE_CURRENT_PASSWORD_ = 3;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		DataBase db = new DataBase();
-		db.connect();
-		
-		PasswordHandler ph = new PasswordHandler();
-		if (db.setPassword(ph.hashPassword(session.getAttribute("password")))) {//Gets the password attribute, hashes the password, sets the password.
-			//change successful
-			request.setAttribute("errorMessage", true);
-		} else {
-			//change not successful
-			request.setAttribute("errorMessage", false);
-		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		DataBase db = new DataBase();
+		db.connect();
+		String username = (String) session.getAttribute("username");
+		String newPw = (String)session.getAttribute("password");
+		String oldPw = (String)session.getAttribute("oldPassword");
+		UserBean ub = new UserBean();
+		ub.populateBean(username, oldPw);
+		if(db.checkLogin(ub)) {
+			if (!newPw.equals(oldPw)) {
+				if (db.changePassword(username ,newPw)) {//sets the password.
+					//change successful
+					request.setAttribute("passwordMessage", PW_CHANGE_SUCCESSFUL_);
+				} else {
+					//change not successful
+					request.setAttribute("passwordMessage", PW_CHANGE_FAILED_NETWORK_ERROR_);
+				}
+			} else {
+				request.setAttribute("passwordMessage", PW_CHANGE_FAILED_IDENTICAL_PASSWORDS_);
+			}
+		} else {
+			request.setAttribute("passwordMessage", PW_CHANGE_FAILED_FALSE_CURRENT_PASSWORD_);
+		}
+		response.sendRedirect("changepassword.jsp");
 		doGet(request, response);
 	}
-
 }
