d8ea5c00e4657a7bffdb3e8e0c8f09bb8af1127a
UG_Oscar Johansson <os3111jo-s@student.lu.se>
2021-03-02 10:20:05 +0100

    Created administrationServlet
    Functionality: Add/remove Users

---
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/AdministrationServlet.java
@@ -0,0 +1,63 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.UserManagementBean;
+import database.DataBase;
+import handlers.PasswordHandler;
+
+public class AdministrationServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	HashMap<String, String> memberMap;
+	UserManagementBean umb;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		DataBase db = new DataBase();
+		db.connect();
+		HttpSession session = request.getSession();
+		umb = new UserManagementBean();
+		memberMap = new HashMap<>();
+		ArrayList<String> memberNames = (ArrayList<String>) db.getMembers();
+		for (String s : memberNames) {
+			if(!s.equals("admin")) {
+				memberMap.put(s, db.getEmail(s));
+			}
+		}
+		umb.populateBean(memberMap);
+		session.setAttribute("UserManagementBean", umb);
+		response.sendRedirect("administration.jsp");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		DataBase db = new DataBase();
+		db.connect();
+		
+		switch(request.getParameter("Add Or Remove")) {
+			case "Add": db.addUser(request.getParameter("name"), PasswordHandler.generatePassword(), 
+						request.getParameter("Email"), PasswordHandler.generateSalt());
+						break;
+			
+			case "Remove": db.removeUser(request.getParameter("name"));
+						   break;
+			default: break;
+		}
+		doGet(request, response);
+		
+	}
+}
+
+
