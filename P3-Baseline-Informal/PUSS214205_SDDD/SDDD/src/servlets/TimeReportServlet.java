5eca8a798f2c45d04504a6451dc25e7651ddf340
UG_Oscar Johansson <os3111jo-s@student.lu.se>
2021-03-05 16:08:26 +0100

    Moved all helper methods from timereportservlet to ServletHandler

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/TimeReportServlet.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/TimeReportServlet.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpSession;
 
 import beans.TimeReportBean;
 import database.DataBase;
+import handlers.ServletHandler;
 
 /**
  * This servlet handles most managements for time report handling-
@@ -34,8 +35,8 @@ public class TimeReportServlet extends ServletBase {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		List<TimeReportBean> timeReports = getTimeReportList(session);
-		List<TimeReportBean> signedReports = getSignedReports();
+		List<TimeReportBean> timeReports = ServletHandler.getTimeReportList(session);
+		List<TimeReportBean> signedReports = ServletHandler.getSignedReports();
 		
 		session.setAttribute("signedReports", signedReports);		// Only signed
 		session.setAttribute("timeReports", timeReports);		// All time reports
@@ -73,11 +74,11 @@ public class TimeReportServlet extends ServletBase {
 		} 
 		else if (editSelectedBtn != null) {
 			session.setAttribute("editable", true);
-			sendViewTimeReportRedirect(request, response, session);
+			ServletHandler.sendViewTimeReportRedirect(request, response, session);
 		} 
 		else if (viewBtn != null) {
 			session.setAttribute("editable", false);
-			sendViewTimeReportRedirect(request, response, session);
+			ServletHandler.sendViewTimeReportRedirect(request, response, session);
 		} 
 		else if (submitNewReport != null) {
 			TimeReportBean bean = new TimeReportBean();
@@ -87,7 +88,7 @@ public class TimeReportServlet extends ServletBase {
 					(String) session.getAttribute("username"), 
 					Integer.parseInt(request.getParameter("week")));
 				if (reportID != 0) {
-					updateReport(request, response, session, db, reportID);
+					ServletHandler.updateReport(request, response, session, db, reportID);
 					doGet(request,response);
 					session.setAttribute("reportError", 0);
 				} else {
@@ -103,11 +104,11 @@ public class TimeReportServlet extends ServletBase {
 			TimeReportBean bean = new TimeReportBean();
 			bean.populateBean(request, response);
 			int reportID = Integer.parseInt(request.getParameter("reportID"));
-			updateReport(request, response, session, db, reportID);
+			ServletHandler.updateReport(request, response, session, db, reportID);
 			doGet(request,response);
 		}
 		else if (getUsersReport != null) {
-			List<TimeReportBean> usersTimeReports = getTimeReportList(getUsersReport);
+			List<TimeReportBean> usersTimeReports = ServletHandler.getTimeReportList(getUsersReport);
 			
 			session.setAttribute("timeReports", usersTimeReports);
 			response.sendRedirect("summaryreport.jsp");
@@ -115,161 +116,5 @@ public class TimeReportServlet extends ServletBase {
 	}
 	
 	
-	/** Helper method to get all unsigned report .*/
-	private List<TimeReportBean> getTimeReportList(HttpSession session) {
-		List<Integer> reportIdList = db.getTimeReportIDs((String) session.getAttribute("username"));
-		List<TimeReportBean> timeReports = new ArrayList<TimeReportBean>();
-		
-		for(Integer id : reportIdList) {
-			TimeReportBean bean = new TimeReportBean();
-			bean.setUsername((String)session.getAttribute("username"));
-			bean.setReportID(id);
-			bean.setSigned(db.getSignatureFromTimeReport(id));
-			bean.setTotalTime(db.getTotalMinutesFromTimeReport(id));
-			bean.setWeek(db.getWeekFromTimeReport(id));
-			timeReports.add(bean);			
-		}
-		
-		// Sorts the list by week, from lowest to highest. Can handle null values
-		timeReports.sort(Comparator.comparing(b -> b.getWeek(), 
-						 Comparator.nullsFirst(Comparator.naturalOrder())));
-		return timeReports;
-	}
-	
-	/** Helper method to get all unsigned report .*/
-	private List<TimeReportBean> getTimeReportList(String userName) {
-		List<Integer> reportIdList = db.getTimeReportIDs(userName);
-		List<TimeReportBean> timeReports = new ArrayList<TimeReportBean>();
-		
-		for(Integer id : reportIdList) {
-			TimeReportBean bean = new TimeReportBean();
-			bean.setUsername(userName);
-			bean.setReportID(id);
-			bean.setSigned(db.getSignatureFromTimeReport(id));
-			bean.setTotalTime(db.getTotalMinutesFromTimeReport(id));
-			bean.setWeek(db.getWeekFromTimeReport(id));
-			timeReports.add(bean);			
-		}
-		
-		// Sorts the list by week, from lowest to highest. Can handle null values
-		timeReports.sort(Comparator.comparing(b -> b.getWeek(), 
-						 Comparator.nullsFirst(Comparator.naturalOrder())));
-		return timeReports;
-	}
-	
-	
-	/** Helper method to get all signed report .*/
-	private List<TimeReportBean> getSignedReports() {
-		List<TimeReportBean> signedReports = new ArrayList<TimeReportBean>();
-		List<Integer> signedTimeReportIDs = db.getSignedTimeReportIDs();
-		
-		for (Integer x: signedTimeReportIDs) {
-			TimeReportBean signedBean = new TimeReportBean();
-			signedBean.setReportID(x);
-			signedBean.setTotalTime(db.getTotalMinutesFromTimeReport(x));
-			signedBean.setWeek(db.getWeekFromTimeReport(x));
-			signedBean.setSigned(db.getSignatureFromTimeReport(x));
-			signedBean.setUsername(db.getUserNameFromTimeReport(x));
-			signedReports.add(signedBean);
-		}
-		// Sorts the list by week, from lowest to highest. Can handle null values
-		signedReports.sort(Comparator.comparing(b -> b.getWeek(), 
-				Comparator.nullsFirst(Comparator.naturalOrder())));
-		return signedReports;
-	}
-
-	/** Translates the Map from the Frontend, to the map that the database
-	 *  expects. This is required since Frontend and DB uses different key names.
-	 * 	The field names of the Frontend can be found in TimeReportBean.java and the db in Database.java
-	 */
-	private Map<String, Integer> translateFrontendToDb(Map<String, Integer> map ) {
-		Map<String, Integer> translated = new HashMap<>();
-		for (Map.Entry<String, Integer> entry: map.entrySet()) {
-			String document = entry.getKey().split("_")[0];
-			if (document.equals("total")) {
-				document = "totalMinutes";
-			} else if (document.equals("final")) {
-				document = "finalReport";
-			} else {
-				document = document.toUpperCase();
-			}
-			translated.put(document, entry.getValue());
-		}
-		return translated;
-	}
-
-	/** Translates the Map from the Database, to the map that the Frontend
-	 *  expects. This is required since Frontend and DB uses different key names.
-	 *  The field names of the Frontend can be found in TimeReportBean.java and the db in Database.java
-	 */
-	private Map<String, Integer> translateDbToFrontend(Map<String, Integer> map, char character ) {
-		Map<String, Integer> translated = new HashMap<>();
-		for (Map.Entry<String, Integer> entry: map.entrySet()) {
-			String document = entry.getKey().split("_")[0];
-
-			if (document.equals("totalMinutes")) {
-				document = String.format("total_%s", character);
-			} else if (document.equals("finalReport")) {
-				document = String.format("final_%s", character);
-			} else {
-				document = String.format("%s_%s", document.toLowerCase(), character);
-			}
-
-			translated.put(document, entry.getValue());
-		}
-		return translated;
-	}
-
-	/** Helper method that redirects the client to view a time report. */
-	private void sendViewTimeReportRedirect(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
-		TimeReportBean bean = getTimeReportBean(request, session);
-		session.setAttribute("timereport", bean);
-		sendJsResponse(response);
-	}
-
-	/** Helper method to send response to frontend JS.
-	 *  Not that this is needed instead of a redirect because the
-	 *  frontend handles the page redirect.
-	 */
-	private void sendJsResponse(HttpServletResponse response) throws IOException {
-		ServletOutputStream out = response.getOutputStream();
-		out.print("ok");
-		out.flush();
-	}
-
-	/** Helper method to get and fill a time report beam. */
-	private TimeReportBean getTimeReportBean(HttpServletRequest request, HttpSession session) {
-		int reportID = Integer.parseInt(request.getParameter("reportID"));
-
-		TimeReportBean bean = new TimeReportBean();
-		bean.populateBean(
-				translateDbToFrontend(db.getDocumentTimeD(reportID), 'd'),
-				translateDbToFrontend(db.getDocumentTimeI(reportID), 'i'),
-				translateDbToFrontend(db.getDocumentTimeF(reportID), 'f'),
-				translateDbToFrontend(db.getDocumentTimeR(reportID), 'r'),
-				db.getActivityReport(reportID),
-				db.getTotalMinutesFromTimeReport(reportID)
-				);
-		bean.setReportID(reportID);
-		bean.setWeek(db.getWeekFromTimeReport(reportID));
-		bean.setUsername(db.getUserNameFromTimeReport(reportID));
-		bean.setRole(db.getRole(bean.getUsername()));
-
-		return bean;
-	}
-	
-	/** Helper method to update a time report. */
-	private void updateReport(HttpServletRequest request, HttpServletResponse response, 
-						      HttpSession session, DataBase db, int reportID) throws ServletException, IOException {
-		TimeReportBean bean = new TimeReportBean();
-		bean.populateBean(request, response);
-		
-		db.updateDocumentTimeD(reportID, translateFrontendToDb(bean.getReportValuesD()));
-		db.updateDocumentTimeI(reportID, translateFrontendToDb(bean.getReportValuesI()));
-		db.updateDocumentTimeF(reportID, translateFrontendToDb(bean.getReportValuesF()));
-		db.updateDocumentTimeR(reportID, translateFrontendToDb(bean.getReportValuesR()));
-		db.updateActivityReport(reportID, bean.getReportValuesActivity());
-		db.updateTotalMinutes(reportID, bean.getTotalTime());
-	}
 
 }
