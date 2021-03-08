721c20d07c26b5c2a263ba3db04c3042a61d525d
abdtaha <78154799+abdtaha@users.noreply.github.com>
2021-03-08 15:56:51 +0100

    Delete unused imports

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/ServletBase.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/ServletBase.java
@@ -1,20 +1,7 @@
 package servlets;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import beans.TimeReportBean;
 import database.DataBase;
 
 /**
@@ -55,7 +42,5 @@ public class ServletBase extends HttpServlet {
 		db = new DataBase();
 		db.connect();
 	}
-
-	
 
 }
