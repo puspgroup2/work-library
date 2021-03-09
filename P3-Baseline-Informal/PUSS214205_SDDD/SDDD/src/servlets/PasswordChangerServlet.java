a2938b5ee91824c6c3a6986c70f81678a4fb5132
HjalleNalle <78014028+HjalleNalle@users.noreply.github.com>
2021-03-09 12:18:45 +0100

    Update PasswordChangerServlet.java

--- P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/PasswordChangerServlet.java
+++ P3-Baseline-Informal/PUSS214205_SDDD/SDDD/src/servlets/PasswordChangerServlet.java
@@ -30,6 +30,8 @@ public class PasswordChangerServlet extends ServletBase {
 	 * 
 	 * @throws ServletException if interference with normal operations occurs.
 	 * @throws IOException if wrong input is received.
+	 * @param request a HttpServletRequest which contains session data
+	 * @param response a HttpServletResponse which is used to send redirects to the user
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
